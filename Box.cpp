5d2dd6d1118656820702bffd47b9585f6953fa85
sahan bandara <99611254+IT21229398@users.noreply.github.com>
2022-04-16 15:48:30 +0000

    T6

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,27 @@
 #include "Box.h"
 
 // Implement setters and getters
+void Box::setLengthnWidth(int len,int wid,int h)
+{
+  length=len;
+  width=wid;
+  height=h;
+}
+void Box::getLength()
+{
+  return length;
+}
+void Box::getWidth()
+{
+  return width;
+}
 
+void Box::hetHeight()
+{
+  return width;
+}
 // Implemenet the calcVolume() unction
-int Box::calcVolume() {
+int Box::calcVolume()
+{
+  return height*length*width;
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,14 @@
 class Student {
   // private section
+  private:
   //    int studentId
+    int studentId;
   //    name <- 20 charcters
+    char name[];
   
   // public section
   //    assignDetails() method declaration
+    void assignDetails(int id,char pname[]);
   //    display() method declaration
+    void display();
 };
